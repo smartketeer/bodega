d89e26a807a9f70a58c87501bcec256a2d8122d1
smartketeer <vincentsedimo03@gmail.com>
2026-06-13 18:14:49 +0800

    Restore dual database setup for POS syncing

--- config/database.php
+++ config/database.php
@@ -64,6 +64,26 @@ return [
             ]) : [],
         ],
 
+        'boutique_pos' => [
+            'driver' => 'mysql',
+            'url' => env('POS_DB_URL'),
+            'host' => env('POS_DB_HOST', '127.0.0.1'),
+            'port' => env('POS_DB_PORT', '3306'),
+            'database' => env('POS_DB_DATABASE', 'boutique_pos_db'),
+            'username' => env('POS_DB_USERNAME', 'root'),
+            'password' => env('POS_DB_PASSWORD', ''),
+            'unix_socket' => env('POS_DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
 
 
         'mariadb' => [

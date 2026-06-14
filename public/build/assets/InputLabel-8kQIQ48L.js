dc57510ce2561c983458f3f3bd6e64cc81e832ea
smartketeer <vincentsedimo03@gmail.com>
2026-06-14 15:20:10 +0800

    feat: add requisition rejection and notifications

--- public/build/assets/InputLabel-8kQIQ48L.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./app-o7e8h7X9.js";var t=e();function n({value:e,className:n=``,children:r,...i}){return(0,t.jsx)(`label`,{...i,className:`block text-sm font-medium text-gray-700 `+n,children:e||r})}export{n as t};

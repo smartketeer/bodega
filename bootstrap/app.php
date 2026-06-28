2e9001ff9f03ceca95b96f9e71b011ae827964e8
smartketeer <vincentsedimo03@gmail.com>
2026-06-28 11:28:41 +0800

    fix: redirect guests to bodega login instead of POS login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        $middleware->redirectGuestsTo(fn () => 'https://boutique-pos.com/login');
+        $middleware->redirectGuestsTo(fn () => route('login'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
